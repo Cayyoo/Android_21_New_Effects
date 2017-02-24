1242bb5f5b1620b10976b37232f5e41f227f0968
ykmeory@163.com <ykmeory@163.com>
2017-02-24 14:45:52 +0800

    Initial commit

---
+++ app/src/main/java/com/effect/zy/android50effect/base/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.effect.zy.android50effect.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+
+import org.xutils.x;
+
+/**
+ * Activity 基类..
+ */
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        x.view().inject(this);
+    }
+
+    public static void reSetTitle(Activity activity) {
+        String st =  activity.getTitle().toString();
+
+        if (!TextUtils.isEmpty(st)) {
+            String[] sp = st.split("/");
+
+            if (sp.length > 0) {
+                activity.setTitle(sp[sp.length - 1]);
+            }
+        }
+
+    }
+
+}
